48bbb6cc17959981b57f4ca37003d80ffc5d9a1d
SquaredR98 <mail@ravi-ranjan.in>
2023-08-16 23:15:39 +0530

    Added buttons to the page

--- app/page.tsx
+++ app/page.tsx
@@ -1,51 +1,40 @@
 import Image from 'next/image';
 
 export default function Home() {
-
-  // useEffect(() => {
-	// 	const lineElements = containerRef.current.querySelectorAll('li');
-	// 	const resume = document.getElementById('resume');
-	// 	const landingCta = document.getElementById('landing-cta');
-	// 	lineElements.forEach((lineElement, index) => {
-	// 		const delay = (index + 1) * 0.2;
-	// 		console.log(delay);
-	// 		lineElement.style.animationDelay = `${delay}s`;
-	// 		lineElement.classList.add('animate-slide-in');
-	// 	});
-	// 	resume.style.animationDelay = `${1}s`;
-	// 	resume.classList.add('animate-slide-in');
-	// 	landingCta.style.animationDelay = `${2}s`;
-	// 	landingCta.classList.add('animate-slide-in');
-
-	// 	setTimeout(() => {
-	// 		lineElements.forEach((lineElement) => {
-	// 			lineElement.classList.remove('opacity-0');
-	// 		});
-	// 	}, intro.length * 0.2 * 1000);
-	// 	setTimeout(() => {
-	// 		resume.classList.remove('opacity-0');
-	// 	}, intro.length * 0.2 * 1000 + 900);
-	// 	setTimeout(() => {
-	// 		landingCta.classList.remove('opacity-0');
-	// 	}, intro.length * 0.2 * 1000 + 1600);
-	// }, []);
-
 	return (
 		<main className='bg-white dark:bg-slate-900 h-screen w-10/12 mx-auto'>
-			<div className='flex items-center h-full'>
-				<div className='w-1/2'>
-					<h1 className='text-slate-950 dark:text-cyan-300 text-6xl font-medium text-shadow-sm'>
-						Ravi Ranjan
-					</h1>
-					<h3 id='' className='text-slate-950 dark:text-cyan-300 text-xl font-light text-shadow-sm'>Software Developer @ Antino Labs</h3>
+			<div className='flex flex-col-reverse items-center h-3/4 lg:h-full md:flex-row'>
+				<div className='md:w-1/2'>
+					<div className='animate-slide-in'>
+						<h1 className='text-slate-950 dark:text-cyan-300 text-6xl font-medium'>
+							Ravi Ranjan
+						</h1>
+						<h3
+							id=''
+							className='text-slate-950 dark:text-cyan-300 text-xl font-light'
+						>
+							Software Developer @ Antino Labs
+						</h3>
+					</div>
+					<div className='flex flex-col justify-between md:flex-row md:w-2/3 lg:w-64 mt-6 animate-slide-in-reverse'>
+						<button className='border bg-cyan-300 border-cyan-300 px-6 py-2 transition-all duration-500 hover:bg-slate-900 hover:text-cyan-300'>
+							Know More
+						</button>
+						<button className='border border-cyan-300 px-6 py-2 transition-all duration-500 hover:bg-cyan-300 text-cyan-300 hover:text-slate-900'>
+							Resume
+						</button>
+					</div>
 				</div>
-				<div className='w-1/2 flex justify-center items-center'>
+				<div className='w-1/2 flex justify-center items-center relative animate-image-fade-in'>
 					<Image
 						src='/static/profile.jpg'
 						alt='Profile'
 						height={480}
 						width={480}
-            className='w-2/3 rounded-full'
+						className='w-2/3'
+						style={{
+							clipPath: 'polygon(0% 25%,0% 75%,50% 100%,100% 75%,100% 25%,50% 0);',
+						}}
 					/>
 				</div>
 			</div>
